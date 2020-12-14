e1fa21c9c4b53ed3ac2be4239c0ef6eb61fec62b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-12-13 21:28:04 -0600

    commit tree 1d37018250f5ec94711a7bf0af0654ba341a0860

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(lesson)
+        @knowledge << lesson
+    end
 
 end
